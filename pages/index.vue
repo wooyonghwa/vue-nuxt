a474652ba588ca51671f1a690e4c5d4cedebf630
wooyonghwa <qhdl0224@gmail.com>
2026-02-14 12:43:19 +0900

    Nuxt Component 계층

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page padding>
+    <div class="q-my-xl text-center">
+      <div class="text-h4">All courses</div>
+      <div class="p-mt-sm text-grey-8">
+        웹 개발 입문부터 학습까지 학습해보세요!
+      </div>
+    </div>
+    <div class="row q-col-gutter-lg">
+      <div v-for="n in 3" :key="n" class="col-12 col-md-4 col-sm-6">
+        <CourseCard />
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
