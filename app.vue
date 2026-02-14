761d3fe41a37c3f5433a0aa6a40d03954508485f
wooyonghwa <qhdl0224@gmail.com>
2026-02-14 09:11:39 +0900

    quasar 적용

--- app.vue
+++ app.vue
@@ -1,8 +1,20 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <q-layout view="hHh lpR fFf" class="bg-grey-2">
+    <q-header elevated class="bg-dark text-white">
+      <q-toolbar>
+        <q-toolbar-title> Learn Nuxt3 </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+    <q-page-container :style="pageContainerStyle">
+      <q-page padding>
+        <div class="text-h2">Learn Nuxt 3</div>
+      </q-page>
+    </q-page-container>
+  </q-layout>
 </template>
 <script setup lang="ts">
-console.log('Hello Nuxt3!');
+const pageContainerStyle = computed(() => ({
+  maxWidth: '1080px',
+  margin: '0 auto',
+}));
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   typescript: {
     shim: false,
     typeCheck: true,
+  },
+
+  modules: ['nuxt-quasar-ui'],
+  quasar: {
+    /* */
   },
 });
