96b8f8aca6907a994d6674928b2cf535369e79fc
wooyonghwa <qhdl0224@gmail.com>
2026-02-20 22:28:48 +0900

    NuxtLink

--- pages/about.vue
+++ pages/about.vue
@@ -5,6 +5,13 @@
       <p class="q-mt-sm text-grey-8">
         Vue & Nuxt Mastery Class 웹은 Nuxt3로 만들어졌습니다.
       </p>
+      <div class="column">
+        <RouterLink to="/">RouterLink Home</RouterLink>
+        <RouterLink to="https://www.naver.com">RouterLink Naver</RouterLink>
+        <br />
+        <NuxtLink prefetched-class="bg-yellow" to="/">NuxtLink Home</NuxtLink>
+        <NuxtLink to="https://www.naver.com">NuxtLink Naver</NuxtLink>
+      </div>
     </div>
   </q-page>
 </template>
