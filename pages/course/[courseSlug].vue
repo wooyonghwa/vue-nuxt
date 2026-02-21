29843449f99fb27043b276332b72cb3060a94bcb
wooyonghwa <qhdl0224@gmail.com>
2026-02-21 21:47:22 +0900

    navigateTogo

--- pages/course/[courseSlug].vue
+++ pages/course/[courseSlug].vue
@@ -73,7 +73,7 @@
             label="이전 강의"
             color="primary"
             unelevated
-            :to="prevCourse.path"
+            @click="movePage(prevCourse.path)"
           />
           <q-btn
             label="쿼리 추가"
@@ -87,7 +87,7 @@
             label="다음 강의"
             color="primary"
             unelevated
-            :to="nextCourse.path"
+            @click="movePage(nextCourse.path)"
           />
         </ClientOnly>
       </template>
@@ -113,6 +113,10 @@ console.log('[route.meta]', route.meta);
 
 const memo = ref('');
 const completed = ref(false);
+
+const movePage = async (path: string) => {
+  await navigateTo(path);
+};
 </script>
 
 <style scoped></style>
