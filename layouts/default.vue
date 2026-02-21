e43094ae10b69ce946c94c08898d2ec78d193bd4
wooyonghwa <qhdl0224@gmail.com>
2026-02-21 21:47:22 +0900

    navigateTogo

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,6 +11,8 @@
           <q-btn stretch flat label="About" no-caps @click="navigate()" />
         </NuxtLink>
         <q-separator dark vertical />
+        <q-btn stretch flat label="Youtube" no-caps @click="moveYoutube" />
+        <q-separator dark vertical />
         <NuxtLink v-slot="{ navigate }" custom to="/admin">
           <q-btn stretch flat label="Admin" no-caps @click="navigate()" />
         </NuxtLink>
@@ -26,4 +28,11 @@ const pageContainerStyle = computed(() => ({
   maxWidth: '1080px',
   margin: '0 auto',
 }));
+
+const moveYoutube = async () => {
+  await navigateTo('https://youtube.com', {
+    external: true,
+    open: { target: '_blank' },
+  });
+};
 </script>
