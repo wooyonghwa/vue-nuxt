578a7184430d8d6a5558080128822e5bb5bd292e
wooyonghwa <qhdl0224@gmail.com>
2026-02-22 14:10:45 +0900

    nuxt runtime app context

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,18 +4,18 @@
       <q-toolbar>
         <q-toolbar-title> Vue & Nuxt Mastery Class </q-toolbar-title>
         <NuxtLink v-slot="{ navigate }" custom to="/">
-          <q-btn stretch flat label="Home" no-caps @click="navigate" />
+          <q-btn stretch flat label="Home" no-caps @click="() => navigate()" />
         </NuxtLink>
         <q-separator dark vertical />
         <NuxtLink v-slot="{ navigate }" custom to="/about">
-          <q-btn stretch flat label="About" no-caps @click="navigate" />
+          <q-btn stretch flat label="About" no-caps @click="() => navigate()" />
         </NuxtLink>
         <q-separator dark vertical />
 
         <q-btn stretch flat label="Youtube" no-caps @click="moveYoutube" />
         <q-separator dark vertical />
         <NuxtLink v-slot="{ navigate }" custom to="/admin">
-          <q-btn stretch flat label="Admin" no-caps @click="navigate" />
+          <q-btn stretch flat label="Admin" no-caps @click="() => navigate()" />
         </NuxtLink>
       </q-toolbar>
     </q-header>
--- pages/course.vue
+++ pages/course.vue
@@ -3,7 +3,7 @@
     <div class="row q-col-gutter-md">
       <div class="col-3">
         <q-card>
-          <q-item-label header>강의 로드맵</q-item-label>
+          <q-item-label header>강의 로드맵 {{ $hello('wooyom') }}</q-item-label>
           <q-list bordered separator>
             <q-item
               v-for="(course, index) in courses"
