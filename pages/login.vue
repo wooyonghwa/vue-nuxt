fa7d36ce0ddae38d947ecac8e82a4a883c544b53
wooyonghwa <qhdl0224@gmail.com>
2026-02-28 10:07:06 +0900

    route middleware & login form

---
+++ pages/login.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page padding>
+    <div class="flex flex-center column q-pt-xl">
+      <PageAuthTitle title="Login" />
+      <PageAuthDescription description="Hello :)" />
+      <FormLogin style="width: 400px" />
+    </div>
+  </q-page>
+</template>
+
+<script setup lang="ts"></script>
